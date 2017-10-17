4dc89276261e5cfee7464d615ee6091fb8dd7912
Jun Li <ptkyagof@umich.edu>
2017-10-17 12:00:24 -0400

    Added cut

--- do-uniq-species.sh
+++ do-uniq-species.sh
@@ -5,5 +5,6 @@
 # Loop over all files
 for data_file in $@
 	do 
-	echo "Unique species in $data-file"
-
+	echo "Unique species in $data_file"
+	# Extract species names for data_file
+	cut -d , -f 2 $data_file 
